14b1c3b0e395bbdf6d9b074296702cb871e0aa41
Jamil Jay <jamiljaymail@yahoo.com>
2022-08-26 08:48:52 -0400

    Adds recipe api

--- backend/src/routes/recipe.ts
+++ backend/src/routes/recipe.ts
@@ -1,9 +1,20 @@
 import { Request, Response, NextFunction } from "express"
+import { RecipeModel } from "../models"
 
 export const recipeMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> => {
-  // TODO fetch and return a recipe
+  const { recipeId } = req.params
+  try {
+    const foundRecipe = await RecipeModel.findById(recipeId)
+    if (foundRecipe) {
+      res.send(foundRecipe)
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (e) {
+    next(e)
+  }
 }
