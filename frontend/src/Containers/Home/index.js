390cb442df2df79364c5abf0a5b8612849a7805c
Jamil Jay <jamiljaymail@yahoo.com>
2022-08-26 11:04:27 -0400

    Update to preserve search between refresh

--- frontend/src/Containers/Home/index.js
+++ frontend/src/Containers/Home/index.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react"
+import React, { useState, useEffect } from "react"
 import { connect } from "react-redux"
 import { bindActionCreators } from "redux"
-import { Link } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom"
 import { HomeWrapper } from "./styles"
 import Input from "@material-ui/core/Input"
 import Checkbox from "@material-ui/core/Checkbox"
@@ -15,81 +15,72 @@ import ListItemText from "@material-ui/core/ListItemText"
 import * as actions from "../../actions"
 
 const INGREDIENT_LIST = ["flour", "sugar", "salt", "butter", "milk"]
-const DEFAULT_INGREDIENTS = JSON.parse(JSON.stringify(INGREDIENT_LIST));
 
-class Home extends Component {
-  constructor(props) {
-    super(props)
-    this.handleSearch = this.handleSearch.bind(this)
-    this.handleIngredient = this.handleIngredient.bind(this)
-    this.fetchSearch = this.fetchSearch.bind(this)
-    this.state = {
-      term: "",
-      ingredients: DEFAULT_INGREDIENTS,
-    }
-  }
-  fetchSearch() {
-    const { searchRecipes } = this.props
-    const { term, ingredients } = this.state
-    searchRecipes(term, ingredients);
-  }
-  handleSearch(event) {
-    const term = event.target.value
-    this.setState({ term })
-  }
-  handleIngredient(ingredient, event) {
-    const { ingredients } = { ...this.state }
+const Home = ({ isLoading, recipes, searchRecipes }) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [term, setTerm] = useState('')
+  const [ingredients, setIngredients] = useState(INGREDIENT_LIST)
+  const fetchSearch = () => searchRecipes(term, ingredients)
+  const ingredientChange = (ingredient, event) => {
     if (event.target.checked) {
-      ingredients.push(ingredient)
+      setIngredients([...ingredients, ingredient])
     } else {
-      const foundIngredient = ingredients.indexOf(ingredient)
-      ingredients.splice(foundIngredient, 1)
+      setIngredients(ingredients.filter((i) => i !== ingredient))
     }
-    this.setState({ ingredients })
   }
-  render() {
-    const { term, ingredients } = this.state
-    const { recipes, isLoading } = this.props
-    return (
-      <HomeWrapper>
-        <Input
-          autoFocus={true}
-          fullWidth={true}
-          onChange={this.handleSearch}
-          value={term}
-        />
-        <div>
-          <h3>Ingredients on hand</h3>
-          {INGREDIENT_LIST.map((ingredient) => (
-            <FormControlLabel
-              key={ingredient}
-              control={
-                <Checkbox
-                  checked={ingredients.includes(ingredient)}
-                  onChange={this.handleIngredient.bind(this, ingredient)}
-                  value={ingredient}
-                />
-              }
-              label={ingredient}
-            />
+
+  useEffect(() => {
+    const paramTerm = searchParams.get('term')
+    const paramIngredients = searchParams.get('ingredients')
+    if (paramTerm) setTerm(paramTerm)
+    if (paramIngredients) setIngredients(searchParams.get('ingredients').split(','))
+  }, [])
+
+  useEffect(() => {
+    searchParams.set('term', term);
+    searchParams.set('ingredients', ingredients);
+    setSearchParams(searchParams);
+  }, [term, ingredients])
+
+  return (
+    <HomeWrapper>
+      <Input
+        autoFocus={true}
+        fullWidth={true}
+        onChange={(e) => setTerm(e.target.value)}
+        value={term}
+      />
+      <div>
+        <h3>Ingredients on hand</h3>
+        {INGREDIENT_LIST.map((ingredient) => (
+          <FormControlLabel
+            key={ingredient}
+            control={
+              <Checkbox
+                checked={ingredients.includes(ingredient)}
+                onChange={(e) => ingredientChange(ingredient, e)}
+                value={ingredient}
+              />
+            }
+            label={ingredient}
+          />
+        ))}
+      </div>
+      <Button onClick={fetchSearch}>search</Button>
+      <Divider />
+      {recipes && (
+        <List>
+          {recipes.map((recipe) => (
+            <ListItem component={Link} to={`/${recipe.id}`} key={recipe.id}>
+              <ListItemText primary={recipe.name} />
+            </ListItem>
           ))}
-        </div>
-        <Button onClick={this.fetchSearch}>search</Button>
-        <Divider />
-        {recipes && (
-          <List>
-            {recipes.map((recipe) => (
-              <ListItem component={Link} to={`/${recipe.id}`} key={recipe.id}>
-                <ListItemText primary={recipe.name} />
-              </ListItem>
-            ))}
-          </List>
-        )}
-        {isLoading && <LinearProgress />}
-        <Divider />
-      </HomeWrapper>
-    )
-  }
+        </List>
+      )}
+      {isLoading && <LinearProgress />}
+      <Divider />
+    </HomeWrapper>
+  )
 }
 
 const mapStateToProps = (state) => {
